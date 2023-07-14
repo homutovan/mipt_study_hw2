c8ab192415515be20e9cc4dcf36be2539f573fb9
Andrey Khomutov <homutovan@live.com>
2023-07-14 13:59:44 +0700

    add bs4

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from db.controller import Controller
 from settings import DB_URI, VERBOSE
+import httpx
+from bs4 import BeautifulSoup
+
+from pprint import pprint
 
 
 def init() -> Controller:
@@ -13,4 +17,15 @@ if __name__ == '__main__':
 
     # logger = get_logger(__name__)
 
-    controller = init()
+    # controller = init()
+
+    resp = httpx.get('https://novosibirsk.hh.ru/vacancies/middle-python-developer')
+
+    if resp.is_success:
+        # pprint(dir(resp))
+        # pprint(resp.content.decode())
+        soup = BeautifulSoup(resp.content.decode(), 'lxml')
+        # print(soup.prettify())
+        # print(dir(soup))
+        res = soup.find('div', attrs={'data-qa': 'vacancy-serp__vacancy vacancy-serp__vacancy_standard'})
+        print(res.prettify())
